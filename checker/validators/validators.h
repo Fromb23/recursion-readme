6daa9bcb9818783f9e19fa863234cf67c5a50e01
Fromb23 <rombo.programming@gmail.com>
2025-07-29 21:36:54 -0400

    Declare validator APIs - Header interface for task-specific validator functions - Included in checker core

---
+++ checker/validators/validators.h
@@ -0,0 +1,10 @@
+#ifndef VALIDATORS_H
+#define VALIDATORS_H
+
+#include "../checker.h"
+
+int validate_task(Task *task, const char *filepath);
+int validate_recursion_file(const char *filepath);
+int validate_factorial_file(const char *filepath);
+
+#endif
