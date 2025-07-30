696ff5f1f45e5f844fb091c28b3d82422b71859e
Fromb23 <rombo.programming@gmail.com>
2025-07-30 10:55:32 -0400

    Free dynamically allocated memory for tasks - Prevents memory leaks when task structs are released

---
+++ checker/utils/free.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "utils.h"
+
+void free_tasks(Task *tasks, int count) {
+	int i, j;
+	for (i = 0; i < count; i++) {
+		free(tasks[i].task_name);
+		free(tasks[i].expected_path);
+		free(tasks[i].main_file);
+		free(tasks[i].target_file);
+		free(tasks[i].expected_output);
+		for (j = 0; j < tasks[i].file_count; j++) {
+			free(tasks[i].expected_files[j]);
+		}
+	}
+}
