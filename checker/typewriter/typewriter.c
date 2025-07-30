9a85c9be7d74f25096352b888136b35b09da8251
Fromb23 <rombo.programming@gmail.com>
2025-07-30 10:53:49 -0400

    Implement typewriter effect for CLI - Simulates slow-printing text for better UX in error/output display

--- checker/typewriter/typewriter.c
+++ checker/typewriter/typewriter.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include "typewriter.h"
 
-void typewrite(const char *text, unsigned int delay_us)
+void typewrite(unsigned int delay_us, const char *format, ...)
 {
-	while (*text) {
-		printf("%c", *text++);
+	va_list args;
+	char buffer[1024];
+	const char *p;
+
+	va_start(args, format);
+	vsnprintf(buffer, sizeof(buffer), format, args);
+	va_end(args);
+
+	p = buffer;
+
+	while (*p) {
+		printf("%c", *p++);
 		fflush(stdout);
 		usleep(delay_us);
 	}
