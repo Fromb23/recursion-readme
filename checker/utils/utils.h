ee8300952aaab96391cec98ba04fe1a0e0c75852
Fromb23 <rombo.programming@gmail.com>
2025-07-30 10:57:20 -0400

    Declare utility function prototypes - Used across checker and validator modules - Includes cloning, parsing, memory management, and file checks

---
+++ checker/utils/utils.h
@@ -0,0 +1,15 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "../main/checker.h"
+
+char *get_directory_path(const char *filepath, char *output, size_t size);
+int check_task_files(Task *task);
+void free_tasks(Task *tasks, int count);
+int is_valid_git_url(const char *url);
+int check_output(const char *script_path, const char *expected_string);
+void trim_trailing_whitespace(char *str);
+int clone_repo(const char *url, const char *target_dir);
+int load_tasks(const char *json_file, Task *tasks, int *task_count);
+
+#endif
