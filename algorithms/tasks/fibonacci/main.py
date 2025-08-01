ffd827da6bfa0ca85c7979c44a3a847d889faaa6
Fromb23 <rombo.programming@gmail.com>
2025-08-01 08:40:17 -0400

    Added fibonacci algorithm

---
+++ algorithms/tasks/fibonacci/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from fibonacci import fibonacci
+
+if __name__ == "__main__":
+    print("Fibonacci up to 5:")
+    fibonacci(5)
+
+    print("\nFibonacci up to 1:")
+    fibonacci(1)
+
+    print("\nFibonacci up to 0:")
+    fibonacci(0)
