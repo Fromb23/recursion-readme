656157aeae9b70d77f0e85f7d228138ea46913c7
Fromb23 <rombo.programming@gmail.com>
2025-08-28 09:16:50 -0400

    Refactor - Deleted all the test files

--- algorithms/tasks/python_class/1-bank.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-class BankAccount():
-
-    def __init__(self, name, account_number):
-        self._name = name
-        self._account_number = account_number
-        self._balance = 0
